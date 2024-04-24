1c1a0174871e08d3d583d7c747c196b9ae89efff
Artem <plishkan25@gmail.com>
2024-04-25 01:26:00 +0300

    table

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Button from "./components/UI/button/Button";
 import CustomSelect from "./components/customSelect/CustomSelect";
 import Logger from "./components/logger/Logger";
+import Table from "./components/table/Table";
 import Title from "./components/title/Title";
 import { useData } from "./context/Context";
 import "./styles/index.scss";
 
 function App() {
-   const { data, setSelect } = useData();
+   const { data, setSelect, select } = useData();
+
+   console.log(select);
 
    console.log(data);
    return (
@@ -15,6 +18,7 @@ function App() {
          <Title text={"Test"} />
          <Logger />
          <CustomSelect options={data} onSelect={setSelect} />
+         <Table data={data} selected={select} />
       </>
    );
 }
--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,5 +1,47 @@
-const Table = () => {
-   return <div></div>;
+import { useEffect, useState } from "react";
+import "./table.scss";
+
+const Table = ({ data, selected }) => {
+   const [array, setArray] = useState([]);
+
+   useEffect(() => {
+      const newArray = [];
+      data.forEach(({ name, field }) => {
+         if (name === selected) {
+            for (let index = 0; index < field; index++) {
+               newArray.push(index);
+            }
+         }
+      });
+      setArray(newArray);
+   }, [selected]);
+
+   let maxWidth;
+   if (array.length >= 70) {
+      maxWidth = 450;
+   } else if (array.length >= 50) {
+      maxWidth = 400;
+   } else if (array.length <= 15) {
+      maxWidth = 260;
+   } else if (array.length <= 15) {
+      maxWidth = 260;
+   } else {
+      maxWidth = 200;
+   }
+
+   const styles = {
+      maxWidth: `${maxWidth}px`,
+   };
+
+   return (
+      <div style={styles} className="square-field">
+         <div className="row">
+            {array.map((index) => (
+               <div key={index} className="square"></div>
+            ))}
+         </div>
+      </div>
+   );
 };
 
 export default Table;
