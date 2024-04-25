44b5d4a7ceea398503e0b681413ed02b549ae514
Artem <plishkan25@gmail.com>
2024-04-25 18:08:28 +0300

    coordinates from squares

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./table.scss";
 
 const Table = ({ data, selected, start }) => {
@@ -6,10 +6,10 @@ const Table = ({ data, selected, start }) => {
 
    useEffect(() => {
       const newArray = [];
-      data.forEach(({ name, field }) => {
+      data.forEach(({ name, field, id }) => {
          if (name === selected) {
             for (let index = 0; index < field; index++) {
-               newArray.push(index);
+               newArray.push({ id: `${id}-${index}`, index });
             }
          }
       });
@@ -23,8 +23,6 @@ const Table = ({ data, selected, start }) => {
       maxWidth = 400;
    } else if (array.length <= 15) {
       maxWidth = 260;
-   } else if (array.length <= 15) {
-      maxWidth = 260;
    } else {
       maxWidth = 200;
    }
@@ -33,12 +31,29 @@ const Table = ({ data, selected, start }) => {
       maxWidth: `${maxWidth}px`,
    };
 
+   const handleHoverSquares = (event) => {
+      const { id, index } = event.target.dataset;
+      const { field } = data.find(({ name }) => name === selected);
+      const row = Math.floor(index / 5) + 1;
+      const square = (index % field) + 1;
+      const column = (index % 5) + 1;
+      console.log(
+         `Hovered square: id ${id} Row ${row}, Column ${column} Square ${square}`
+      );
+   };
+
    return (
       start && (
          <div style={styles} className="square-field">
             <div className="row">
-               {array.map((index) => (
-                  <div key={index} className="square"></div>
+               {array.map(({ id, index }) => (
+                  <div
+                     key={id}
+                     data-id={id}
+                     data-index={index}
+                     className="square"
+                     onMouseEnter={handleHoverSquares}
+                  ></div>
                ))}
             </div>
          </div>
